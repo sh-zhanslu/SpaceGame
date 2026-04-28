f482f2b16c5c36f1405553db0130c4460be2e154
sh-zhanslu <zhanslu.shakenova06@mail.ru>
2026-04-29 01:02:35 +0600

    Задание4: Реализован класс векторов Vector.cs и тесты VectrTests.cs

---
+++ SpaceGameSolution/SpaceGame.Core/Vector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace SpaceGame.Core
+{
+    public class Vector : IEquatable<Vector>
+    {
+        private readonly int[] _components;
+
+        public Vector(params int[] components)
+        {
+            if (components == null)
+                throw new ArgumentNullException(nameof(components));
+            if (components.Length == 0)
+                throw new ArgumentException("Вектор должен иметь хотя бы один компонент", nameof(components));
+            _components = components.ToArray();
+        }
+
+        public int this[int index] => _components[index];
+        public int Dimension => _components.Length;
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            if (a.Dimension != b.Dimension)
+                throw new ArgumentException("Вектор должен иметь одинаковую размерность");
+            var result = new int[a.Dimension];
+            for (int i = 0; i < a.Dimension; i++)
+                result[i] = a[i] + b[i];
+            return new Vector(result);
+        }
+
+        public override bool Equals(object obj) => Equals(obj as Vector);
+
+        public bool Equals(Vector other)
+        {
+            if (other is null) return false;
+            if (Dimension != other.Dimension) return false;
+            for (int i = 0; i < Dimension; i++)
+                if (_components[i] != other[i]) return false;
+            return true;
+        }
+
+        public static bool operator ==(Vector left, Vector right)
+        {
+            if (ReferenceEquals(left, right)) return true;
+            if (left is null || right is null) return false;
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector left, Vector right) => !(left == right);
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            foreach (var comp in _components)
+                hash = hash * 31 + comp;
+            return hash;
+        }
+    }
+}
---
+++ SpaceGameSolution/SpaceGame.Tests/VectorTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using SpaceGame.Core;
+using Xunit;
+
+namespace SpaceGame.Tests
+{
+    public class VectorTests
+    {
+        [Fact]
+        public void Add_Vectors_ReturnsSum()
+        {
+            var v1 = new Vector(1, -1, 2);
+            var v2 = new Vector(-1, 1, -2);
+            var result = v1 + v2;
+            Assert.Equal(new Vector(0, 0, 0), result);
+        }
+
+        [Fact]
+        public void Add_DifferentDimensions_ThrowsArgumentException()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2);
+            Assert.Throws<ArgumentException>(() => v1 + v2);
+        }
+
+        [Fact]
+        public void Add_DifferentDimensionsReverse_ThrowsArgumentException()
+        {
+            var v1 = new Vector(1, 2);
+            var v2 = new Vector(1, 2, 3);
+            Assert.Throws<ArgumentException>(() => v1 + v2);
+        }
+
+        [Fact]
+        public void Equals_SameComponents_ReturnsTrue()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2, 3);
+            Assert.True(v1.Equals(v2));
+        }
+
+        [Fact]
+        public void EqualsOperator_SameComponents_ReturnsTrue()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2, 3);
+            Assert.True(v1 == v2);
+        }
+
+        [Fact]
+        public void Equals_DifferentComponents_ReturnsFalse()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2, 4);
+            Assert.False(v1.Equals(v2));
+        }
+
+        [Fact]
+        public void NotEqualsOperator_DifferentComponents_ReturnsTrue()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2, 4);
+            Assert.True(v1 != v2);
+        }
+
+        [Fact]
+        public void GetHashCode_ExistsAndIsConsistent()
+        {
+            var v1 = new Vector(1, 2, 3);
+            var v2 = new Vector(1, 2, 3);
+            Assert.NotEqual(0, v1.GetHashCode());
+            Assert.Equal(v1.GetHashCode(), v2.GetHashCode());
+        }
+    }
+}
