37156186e547a32bd790e368945b7ea9b8918b1f
sh-zhanslu <zhanslu.shakenova06@mail.ru>
2026-04-29 01:27:25 +0600

    Задание5: Определена команда Move для равномерного поступательного движения без деформации, созданы: интерфейс IMovingObject.cs класс MoveCommand.cs тесты MoveCommandTests.cs

---
+++ SpaceGameSolution/SpaceGame.Core/MoveCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SpaceGame.Core
+{
+    public class MoveCommand : ICommand
+    {
+        private readonly IMovingObject _movingObject;
+
+        public MoveCommand(IMovingObject movingObject)
+        {
+            _movingObject = movingObject ?? throw new ArgumentNullException(nameof(movingObject));
+        }
+
+        public void Execute()
+        {
+            if (_movingObject.Position == null)
+                throw new InvalidOperationException("Не удаётся получить позицию");
+            if (_movingObject.Velocity == null)
+                throw new InvalidOperationException("Не удаётся набрать скорость");
+
+            try
+            {
+                _movingObject.Position = _movingObject.Position + _movingObject.Velocity;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Не удаётся установить новую позицию", ex);
+            }
+        }
+    }
+}
