bbe9c3415dc6034d121e5b5ea1eecba01b845c03
timothy <caveiroussight@gmail.com>
2026-05-26 21:36:43 +0600

    17

---
+++ SpaceGameSolution/SpaceGame.Core/Commands/CommandInjectableCommand.cs
@@ -0,0 +1,20 @@
+namespace SpaceGame.Core;
+
+public class CommandInjectableCommand : ICommand, ICommandInjectable
+{
+    private ICommand _command;
+
+    public void Inject(ICommand command)
+    {
+        _command = command;
+    }
+
+    public void Execute()
+    {
+        if (_command == null)
+        {
+            throw new Exception();
+        }
+        _command.Execute();
+    }
+}
